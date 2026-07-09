f0b6178b185b2def1bc01b261e768f122459026a
chprr <miki114758@gmail.com>
2026-07-09 14:42:26 +0300

    Refactor database engine initialization
    Updated database initialization to use a configurable data directory for the SQLite database file.

--- db/__init__.py
+++ db/__init__.py
@@ -1,6 +1,12 @@
-from sqlmodel import SQLModel, create_engine
+import os
+from sqlmodel import create_engine, SQLModel
 
-engine = create_engine("sqlite:///database.db")
+# Читаємо шлях до папки з даними (за замовчуванням - поточна папка)
+DATA_DIR = os.getenv("DATA_DIR", ".")
+sqlite_file_name = os.path.join(DATA_DIR, "data.db")
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url)
 
 def init():
-  SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine)
